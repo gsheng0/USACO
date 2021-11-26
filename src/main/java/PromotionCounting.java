472f8a6976badf4a6284a4b7aa2a8b575259b15c
George Sheng <gsheng112@gmail.com>
2021-11-25 23:56:41 -0500

    Finished promotion counting

---
+++ src/main/java/PromotionCounting.java
@@ -0,0 +1,73 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class PromotionCounting {
+    public static void run(){
+        int[] before = new int[4];
+        int[] after = new int[4];
+        int[] difference = new int[4];
+        int[] out = new int[3];
+        for(int i = 0; i < 4; i++){
+            before[i] = r.nextInt();
+            after[i] = r.nextInt();
+            difference[i] = after[i] - before[i];
+        }
+        for(int i = 1; i < difference.length; i++){
+            for(int x = i; x >= 1; x--){
+                out[x - 1] += difference[i];
+            }
+        }
+        print(out);
+
+    }
+    public static void print(int[] arr){
+        for(int i : arr){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+
+
+    private static InputStream createInputStream() throws Exception {
+        Class clazz = PromotionCounting.class;
+        return clazz.getResourceAsStream("/" + clazz.getSimpleName() + ".in");
+    }
+    private static class InputReader {
+        BufferedReader reader;
+        StringTokenizer tokenizer;
+        public InputReader(InputStream stream) {
+            reader = new BufferedReader(new InputStreamReader(stream), 32768);
+            tokenizer = null;
+        }
+        String next() { // reads in the next string
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+        public int nextInt() { // reads in the next int
+            return Integer.parseInt(next());
+        }
+        public long nextLong() { // reads in the next long
+            return Long.parseLong(next());
+        }
+        public double nextDouble() { // reads in the next double
+            return Double.parseDouble(next());
+        }
+    }
+
+    private static InputReader r = new InputReader(System.in);
+    private static PrintWriter pw = new PrintWriter(System.out);
+
+    public static void main(String[] args) throws Exception {
+        // comment out below line when submitting
+        r = new InputReader(createInputStream());
+        run();
+        pw.close();
+    }
+}
